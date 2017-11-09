2532395bd3642f7102b50dc3d549942f13c8a28f
Ferit Tunçer <ferit@lavabit.com>
2017-11-09 19:44:41 +0300

    drafts

--- UnitsNetConverter/UnitsNetConverter/MainWindow.xaml.cs
+++ UnitsNetConverter/UnitsNetConverter/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UnitsNet;
 
 namespace UnitsNetConverter
 {
@@ -24,5 +26,6 @@ namespace UnitsNetConverter
         {
             InitializeComponent();
         }
+
     }
 }
